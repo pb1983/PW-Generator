8dec0808493bcda9a9ba720abce504ea74dd5b4d
pb1983 <phil.bryer@gmail.com>
2023-07-26 18:18:11 -0400

    Added function to generate password and include error messages when character limit is not met. Added error notification when no character types are selected

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 
 let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 let builtArray = [];
 
@@ -17,37 +17,58 @@ let builtArray = [];
 
 function generatePassword() {
 
-  if (confirm("Do you want your password to contain lowercase letters?")) 
-  {
-
-    builtArray = [...builtArray, ...lowerAlphabet];
-
-  }
-
-  let password = "";
-
   let passwordLength = prompt("How long do you want the password?");
 
+  if (passwordLength < 8) {
+  return "Your length must be longer than 8 characters";
+}  else if (passwordLength > 128) {
+    return "Your length must not exceed 120 characters";
+}
+  
+  let upper = confirm("Do you want to include upper case letters?");
+  let lower = confirm("Do you want to include lower case letters?");
+  let numbers = confirm("Do you want to include numbers?");
 
+
+  if (upper && lower && numbers === true)  {
+    builtArray = [...builtArray, ...lowerAlphabet, ...upperAlphabet, ...numbersArray];
+  } else if (upper && lower === true) {
+    builtArray = [...builtArray, ...upperAlphabet, ...lowerAlphabet];
+  } else if (upper && numbers === true) {
+    builtArray = [...builtArray, ...upperAlphabet, ...numbersArray];
+  } else if (lower && numbers === true) {
+    builtArray = [...builtArray, ...lowerAlphabet, ...numbersArray];
+  }  else if (upper === true) {
+    builtArray = [...builtArray, ...upperAlphabet];
+  } else if (numbers === true) {
+    builtArray = [...builtArray, ...numbersArray];
+  } else if (lower === true) {
+    builtArray = [...builtArray, ...lowerAlphabet];
+  } else {
+      return "You must select at least one type of input";
+    }
+ 
+
+  
+  
   for (let i= 0; i < passwordLength; i++) {
-
-    password += "X";
-    
+    let randomIndex = Math.floor(Math.random()*builtArray.length);
+    password += builtArray[randomIndex];
   }
-    
+  
 
+  return password;
 
-//build a string based off of randomly selected values from an array
-
-return password;
 } 
 
 
 
+// generatePassword()
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -55,3 +76,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//Ask user how many characters they want
+//ask user if they want lower case, upper case, and numbers
+//Create error message if they do not have enough characters, too many character, or do not select any uc or lc options
+//Be able to return the number of characters the user wants in their password with the options they selected (ucc lcc and numbers)
+  //Math.floor(Math.random()*builtArray.length) * passwordLength;
+
+   
