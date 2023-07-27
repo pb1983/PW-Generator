d87eabe7b2fee71f0f3a60340a250fef64309d15
pb1983 <phil.bryer@gmail.com>
2023-07-27 16:25:35 -0400

    Updated Readme. Refactored if statements. Fixed issue with event listener sequence.

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,8 @@ let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 
 let numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+let charArray = ["!", "@", "#", "$", "%", "^", "&", "*",];
+
 let builtArray = [];
 
 let password = ""; 
@@ -17,63 +19,56 @@ let password = "";
 
 function generatePassword() {
 
+  password = "";
+  builtArray = [];
   let passwordLength = prompt("How long do you want the password?");
 
   if (passwordLength < 8) {
-  return "Your length must be longer than 8 characters";
+  return "Your length must be longer than 8 characters. Please try again. ";
 }  else if (passwordLength > 128) {
-    return "Your length must not exceed 128 characters";
+    return "Your length must not exceed 128 characters. Please try again.";
 }
   
-  let upper = confirm("Do you want to include upper case letters?");
-  let lower = confirm("Do you want to include lower case letters?");
-  let numbers = confirm("Do you want to include numbers?");
 
-
-  if (upper && lower && numbers)  {
-    builtArray = [...builtArray, ...lowerAlphabet, ...upperAlphabet, ...numbersArray];
-  } else if (upper && lower) {
-    builtArray = [...builtArray, ...upperAlphabet, ...lowerAlphabet];
-  } else if (upper && numbers) {
-    builtArray = [...builtArray, ...upperAlphabet, ...numbersArray];
-  } else if (lower && numbers) {
-    builtArray = [...builtArray, ...lowerAlphabet, ...numbersArray];
-  }  else if (upper) {
+    if (confirm("Do you want to include upper case letters?")) {
     builtArray = [...builtArray, ...upperAlphabet];
-  } else if (numbers) {
-    builtArray = [...builtArray, ...numbersArray];
-  } else if (lower) {
-    builtArray = [...builtArray, ...lowerAlphabet];
-  } else {
-      return "You must select at least one type of input";
     }
- 
 
-  
-  
+    if (confirm("Do you want to include lower case letters?")) {
+      builtArray = [...builtArray, ...lowerAlphabet];
+    }
+
+   if (confirm("Do you want to include numbers?")) {
+      builtArray = [...builtArray, ...numbersArray]; 
+    }
+
+   if (confirm("Do you want special characters?")) {
+      builtArray = [...builtArray, ...charArray];
+    } 
+
+   if (builtArray.length === 0) {
+    return "You must select at least one type of character. Please try again.";
+   }
+
+    
   for (let i= 0; i < passwordLength; i++) {
     let randomIndex = Math.floor(Math.random()*builtArray.length);
     password += builtArray[randomIndex];
   }
   
-
   return password;
 
-} 
-
-password = generatePassword();
+}
 
 
 // Write password to the #password input
 function writePassword() {
-  // password = generatePassword();
+  password = generatePassword();
   console.log(password);
   let passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Random Password Generator
+
+## This application will retrieve input from the user to generate a random password using HTML, CSS, and Javascript:
+
+ * ### The user will be prompted to provide their desired character length. If it is less than 8, or more than 128, they will receive a notification that their input is insufficient, and to try again.
+ * ### The user will be given the option to select upper case letters, lower case letters, numbers, and special characters as the criteria to compose their password. If they do not choose any of these options they will receive a notification that they must select at least one character type, and to try again. 
+ * ### Contactonated arrays, for loops, listener events, and functions were used in order to achieve the desired functionality. 
+
+
+### Starter Code:
+[URL] https://github.com/coding-boot-camp/friendly-parakeet
+
+
+ ### URL and Screenshots:
+ [URL] https://pb1983.github.io/PW-Generator/
